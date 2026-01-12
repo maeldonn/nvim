047ca82a9cc41c746cfa1c376d327b449437cdfe
Maël DONNART <mael.donnart1@gmail.com>
2026-01-12 23:22:38 +0100

    refactor: update some plugins configuration
    Fix an error for treesitter and add keymapings for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,16 @@
 return {
-  'zbirenbaum/copilot.vim',
+  'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
-  event = 'VimEnter',
+  event = 'InsertEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = true,
+  keys = {
+    { '<C-l>', require('copilot.suggestion').accept_line, desc = 'Accept [L]ine' },
+    { '<C-j>', require('copilot.suggestion').accept,      desc = 'Accept suggestion' },
+  },
+  init = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+  config = function()
+    require('copilot').setup {}
+  end,
 }
