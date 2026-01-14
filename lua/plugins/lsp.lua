96d8cffcdd44aa34a342daf73497df9ff52ec3c7
Maël DONNART <mael.donnart1@gmail.com>
2026-01-14 23:15:32 +0100

    refactor(lsp): Update LSP configuration
    Switch to neovim O.11 native LSP and switch completion from nvim-cmp to
    blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,130 @@
 return {
   {
-    'williamboman/mason.nvim',
-    lazy = false,
+    'saghen/blink.cmp',
+    version = '1.*',
+    dependencies = {
+      { 'rafamadriz/friendly-snippets' },
+      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+      { 'onsails/lspkind-nvim' },
+    },
     opts = {
-      ui = {
-        border = 'rounded',
+      snippets = { preset = 'luasnip' },
+      completion = {
+        menu = {
+          border = 'rounded',
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                    local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
+                    end
+                  else
+                    icon = require('lspkind').symbolic(ctx.kind, {
+                      mode = 'symbol',
+                    })
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                    local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
+                end,
+              },
+            },
+          },
+        },
+      },
+      keymap = {
+        preset = 'enter',
+      },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'mason-org/mason-lspconfig.nvim' },
+    },
+    config = function()
+      local function setup_keymaps(bufnr)
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc, silent = true })
+        end
+
+        map('n', 'K', '<cmd>Lspsaga hover_doc ++quiet<CR>', 'Hover documentation')
+        map('n', 'gd', vim.lsp.buf.definition, '[G]o to [D]efinition')
+        map('n', 'gD', vim.lsp.buf.declaration, '[G]o to [D]eclaration')
+        map('n', 'gt', vim.lsp.buf.type_definition, '[G]o to [T]ype definition')
+        map('n', 'gi', vim.lsp.buf.implementation, '[G]o to [I]mplementation')
+        map('n', 'gu', '<cmd>Lspsaga finder<CR>', '[G]o to [U]sage')
+        map('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', 'Previous diagnostic')
+        map('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', 'Next diagnostic')
+        map('n', 'gr', '<cmd>Lspsaga rename<CR>', 'Rename')
+        map('i', '<C-h>', vim.lsp.buf.signature_help, 'Signature help')
+        map('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', '[C]ode [A]ction')
+        map('n', '<leader>so', '<cmd>Lspsaga outline<CR>', '[S]ymbol [O]utline')
+      end
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
+        callback = function(args)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then
+            return
+          end
+
+          setup_keymaps(bufnr)
+        end,
+      })
+
+      vim.diagnostic.config {
+        virtual_text = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '✘',
+            [vim.diagnostic.severity.WARN] = '▲',
+            [vim.diagnostic.severity.HINT] = '⚑',
+            [vim.diagnostic.severity.INFO] = '»',
+          },
+        },
+      }
+    end,
+  },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = {
+        'lua_ls',
+        'gopls',
+        'ts_ls',
+      },
+    },
+    dependencies = {
+      { 'neovim/nvim-lspconfig' },
+      {
+        'mason-org/mason.nvim',
+        opts = {
+          ui = {
+            border = 'rounded',
+          },
+        },
       },
     },
   },
@@ -18,119 +138,5 @@ return {
       lightbulb = { enable = false },
       symbol_in_winbar = { enable = false },
     },
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    lazy = false,
-    dependencies = {
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'L3MON4D3/LuaSnip' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'onsails/lspkind-nvim' },
-      { 'rafamadriz/friendly-snippets' },
-    },
-    config = function()
-      require('luasnip.loaders.from_vscode').lazy_load()
-
-      local cmp = require 'cmp'
-      cmp.setup {
-        sources = {
-          { name = 'path' },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip', keyword_length = 2 },
-          { name = 'buffer', keyword_length = 3 },
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-        },
-        preselect = 'item',
-        completion = {
-          completeopt = 'menu,menuone,noinsert',
-        },
-        formatting = {
-          fields = { 'abbr', 'kind', 'menu' },
-          format = require('lspkind').cmp_format {
-            mode = 'symbol_text',
-            maxwidth = 50,
-            ellipsis_char = '...',
-          },
-        },
-      }
-    end,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-    },
-    config = function()
-      local lspconfig = require 'lspconfig'
-      local lsp_defaults = lspconfig.util.default_config
-
-      lsp_defaults.capabilities = vim.tbl_deep_extend('force', lsp_defaults.capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        desc = 'LSP actions',
-        callback = function(event)
-          local opts = { buffer = event.buf }
-
-          vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc ++quiet<CR>', opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', 'gu', '<cmd>Lspsaga finder<CR>', opts)
-          vim.keymap.set('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
-          vim.keymap.set('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-          vim.keymap.set('n', 'gr', '<cmd>Lspsaga rename<CR>', opts)
-          vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts)
-          vim.keymap.set('n', '<leader>so', '<cmd>Lspsaga outline<CR>', opts)
-        end,
-      })
-
-      -- FIXME: Conform.nvim is overriding signs
-      vim.diagnostic.config {
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '✘',
-            [vim.diagnostic.severity.WARN] = '▲',
-            [vim.diagnostic.severity.HINT] = '⚑',
-            [vim.diagnostic.severity.INFO] = '»',
-          },
-        },
-      }
-
-      require('mason-lspconfig').setup {
-        ensure_installed = {
-          'lua_ls',
-          'gopls',
-          'ts_ls',
-        },
-        handlers = {
-          function(server_name)
-            lspconfig[server_name].setup {}
-          end,
-        },
-      }
-    end,
   },
 }
