b15ff5ee7f71d747ba733aaefc7dffda210005f2
Maël DONNART <mael.donnart1@gmail.com>
2026-02-14 19:29:59 +0100

    fix: Load snippets from friendly snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,9 +3,17 @@ return {
     'saghen/blink.cmp',
     version = '1.*',
     dependencies = {
-      { 'rafamadriz/friendly-snippets' },
-      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
       { 'onsails/lspkind-nvim' },
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+        build = 'make install_jsregexp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = function()
+          require('luasnip').setup()
+          require('luasnip.loaders.from_vscode').lazy_load()
+        end,
+      },
     },
     opts = {
       snippets = { preset = 'luasnip' },
