473e5ed01f3bb00862a77c3667a0eb9311a3290c
Maël DONNART <mael.donnart1@gmail.com>
2026-02-15 18:49:21 +0100

    refactor: Replace telescope with snacks.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -14,5 +14,85 @@ return {
       end,
       desc = 'File Explorer',
     },
+    -- Pickers
+    {
+      '<leader>sf',
+      function()
+        require('snacks.picker').files {
+          layout = 'select',
+          hidden = true,
+          exclude = {
+            '**/node_modules/*',
+            '**/vendor/*',
+            '**/gen/*',
+          },
+        }
+      end,
+      desc = '[S]earch [F]iles',
+    },
+    {
+      '<leader>sF',
+      function()
+        require('snacks.picker').git_files { layout = 'select' }
+      end,
+      desc = '[C-p] Search git files',
+    },
+    {
+      '<leader>sw',
+      function()
+        require('snacks.picker').grep_word {
+          hidden = true,
+          exclude = {
+            '**/node_modules/*',
+            '**/vendor/*',
+            '**/gen/*',
+          },
+        }
+      end,
+      mode = { 'n', 'v', 'x' },
+      desc = '[S]earch current [W]ord',
+    },
+    {
+      '<leader>sg',
+      function()
+        require('snacks.picker').grep {
+          hidden = true,
+          exclude = {
+            '**/node_modules/*',
+            '**/vendor/*',
+            '**/gen/*',
+          },
+        }
+      end,
+      desc = '[S]earch by [G]rep',
+    },
+    {
+      '<leader>sd',
+      function()
+        require('snacks.picker').diagnostics { focus = 'list' }
+      end,
+      desc = '[S]earch [D]iagnostics',
+    },
+    {
+      '<leader>sr',
+      function()
+        require('snacks.picker').resume()
+      end,
+      desc = '[S]earch [R]esume',
+    },
+    {
+      '<leader>gs',
+      function()
+        require('snacks.picker').git_status { focus = 'list' }
+      end,
+      desc = '[G]it [S]tatus',
+    },
+    {
+      '<leader><leader>',
+      function()
+        require('snacks.picker').buffers { layout = 'select', focus = 'list' }
+      end,
+      desc = '[ ] Find existing buffers',
+    },
   },
 }
