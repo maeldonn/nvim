f0ad4c161d68101e5e23c7cc559b8ff71f67ed1f
Maël DONNART <mael.donnart@proton.me>
2026-03-30 12:50:16 +0200

    feat: Update snacks configuration

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   opts = {
     bigfile = {},
+    picker = {},
     explorer = { replace_netrw = true, trash = true },
     input = {},
     quickfile = {},
@@ -14,7 +15,22 @@ return {
     {
       '\\',
       function()
-        require('snacks').explorer()
+        local explorer_win = nil
+
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft = vim.bo[buf].filetype
+          if ft == 'snacks_picker_list' then
+            explorer_win = win
+            break
+          end
+        end
+
+        if vim.api.nvim_get_current_win() ~= explorer_win and explorer_win then
+          vim.api.nvim_set_current_win(explorer_win)
+        else
+          require('snacks').explorer()
+        end
       end,
       desc = 'File Explorer',
     },
