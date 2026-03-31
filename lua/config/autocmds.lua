c2378021d2ffe0101b3253ed5da86af039487e6e
Maël DONNART <mael.donnart@proton.me>
2026-03-31 18:02:39 +0200

    feat: Add autocommands to improve experience

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank { timeout = 40 }
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
+  command = 'wincmd L',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('no_auto_comment', {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove { 'c', 'o' }
+  end,
+})
